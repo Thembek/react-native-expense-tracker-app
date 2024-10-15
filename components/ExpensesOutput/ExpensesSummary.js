eca7d6e347a7e809d79e63ab729b8951a01be31f
Thembek <andile.goba@younglings.africa>
2024-10-15 10:01:03 +0200

    Creating key app components to display expenes commit

---
+++ components/ExpensesOutput/ExpensesSummary.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+function ExpensesSummary(){
+    return(
+        <View>
+            <Text>Period</Text>
+            <Text>$177.99</Text>
+        </View>
+    )
+}
+
+export default ExpensesSummary;
