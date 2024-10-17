e362dcc32e21db45f226be903c9f99e0782ea45e
Thembek <andile.goba@younglings.africa>
2024-10-17 09:58:55 +0200

    adding validations on input commit

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 
 import Input from './Input';
@@ -28,6 +28,15 @@ function ExpenseForm({ submitButtonLabel, onCancel, onSubmit, defaultValues }){
             date: new Date(inputValue.date),
             description: inputValue.description,
         };
+
+        const amountIsValid = ! isNaN(expenseData.amount) && expenseData.amount > 0;
+        const dateIsValid = expenseData.date.toString() !== 'Invalid Date';
+        const descriptionIsValid = expenseData.description.trim().length > 0;
+
+        if(!amountIsValid || !dateIsValid || !descriptionIsValid){
+            Alert.alert('Invalid input', 'Please check your input values')
+            return;
+        }
 
         onSubmit(expenseData);
     }
